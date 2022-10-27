2de4e02688f3a3e3acb72c30d3a1f76f1ee3ddbe
戴海洋 <haiyang.dai@davincimotor.com>
2022-10-27 11:50:57 +0800

    feat: rand

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
+use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     let mut input: String = String::new();
 
-    io::stdin().read_line(&mut input).expect("输入错误");
+    let radom: i32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("您的输入是 {}", input);
+    println!("神秘数字 {}", radom);
 
-    let comper: String = String::new();
+    loop {
+        io::stdin().read_line(&mut input).expect("输入错误");
 
-    match comper.cmp(&input) {
-        Ordering::Less => println!("小于"),
-        Ordering::Greater => println!("大于"),
-        Ordering::Equal => println!("差不多"),
+        let input = input.trim().parse().expect("请输入数字");
+
+        println!("您的输入是 {}", input);
+
+        match radom.cmp(&input) {
+            Ordering::Less => println!("小于"),
+            Ordering::Greater => println!("大于"),
+            Ordering::Equal => println!("差不多"),
+        }
     }
 }

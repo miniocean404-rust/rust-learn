de7e434076895bead1a256b02b995dda5a51554f
戴海洋 <haiyang.dai@davincimotor.com>
2022-10-27 17:24:13 +0800

    feat: enum

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,4 @@
 pub mod use_enum;
+pub mod use_match;
 pub mod use_option;
 pub mod use_struct;
--- src/api/use_match.rs
+++ src/api/use_match.rs
@@ -1,1 +1,26 @@
+#[derive(Debug)]
+enum Mode2 {
+    V1,
+}
 
+enum Mode {
+    V1,
+    V2,
+    Match(Mode2),
+}
+
+fn show_match(mode: Mode) -> u8 {
+    match mode {
+        Mode::V1 => 1,
+        Mode::V2 => 2,
+        Mode::Match(state) => {
+            println!("{:#?}", state);
+            25
+        }
+    }
+}
+
+fn use_show_match() {
+    let enum_value = Mode::Match(Mode2::V1);
+    let value: u8 = show_match(enum_value);
+}

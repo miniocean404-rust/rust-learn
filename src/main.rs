eb6b7e28624726e81e85f213814662a0b4961eb8
戴海洋 <haiyang.dai@davincimotor.com>
2022-10-27 11:56:52 +0800

    feat: rand

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,16 @@ fn main() {
     let mut input: String = String::new();
 
     let radom: i32 = rand::thread_rng().gen_range(1..=100);
-
     println!("神秘数字 {}", radom);
 
     loop {
         io::stdin().read_line(&mut input).expect("输入错误");
 
-        let input = input.trim().parse().expect("请输入数字");
+        let input: i32 = input.trim().parse().expect("请输入数字");
 
         println!("您的输入是 {}", input);
 
-        match radom.cmp(&input) {
+        match input.cmp(&radom) {
             Ordering::Less => println!("小于"),
             Ordering::Greater => println!("大于"),
             Ordering::Equal => println!("差不多"),

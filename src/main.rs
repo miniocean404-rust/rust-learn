a47d158d711789477b122224f21e42398f1f7a73
戴海洋 <haiyang.dai@davincimotor.com>
2022-10-27 10:51:08 +0800

    pref: 删除无用代码

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
-
     let mut input: String = String::new();
 
     io::stdin().read_line(&mut input).expect("输入错误");
 
-    print!("您的输入是 {}", input)
+    println!("您的输入是 {}", input)
 }

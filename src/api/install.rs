2279531ab4d16d63c2923155d41fe1a9563023bd
MiniOcean404 <1037306928@qq.com>
2022-11-07 01:19:11 +0800

    feat: install

---
+++ src/api/install.rs
@@ -0,0 +1,9 @@
+// 安装二进制，安装后在 ~/.cargo/bin
+// cargo install xx
+// 如果 ~/.cargo/bin 在环境变量 Path 下则可直接运行
+// Windows CMD 中查看 echo %PATH%
+
+// 自定义扩展 cargo 像子命令一样
+// 文件格式：cargo-some 则可以运行 cargo some
+// cargo --list 查看所有子命令
+// 这种命令可以通过社区的开发 使用 cargo install 安装，进而扩展 cargo

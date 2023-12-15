5c3cd56127696f9bd02ff4d87b8f999d94f5ee94
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-12-15 16:58:53 +0800

    feat: 转化错误

--- syntax/base/_type/err.rs
+++ syntax/base/_type/err.rs
@@ -11,6 +11,9 @@ use std::{
     io::{self, Read},
 };
 
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
+
 pub fn use_err() -> Result<String, io::Error> {
     let mut s: String = String::new();
     File::open("./hello.txt")?.read_to_string(&mut s)?;
@@ -27,3 +30,49 @@ pub fn use_err() -> Result<String, io::Error> {
 //         }
 //     });
 // }
+
+//! 将错误转化：
+// 此处的 From::from 调用意味着我们尝试将错误类型转换为 函数返回的类型。这使得很容易将错误封装到 更高级别的错误。
+/// ```rs
+/// match expression {
+///     Ok(value) => value,
+///     Err(err)  => return Err(From::from(err)),
+/// }
+/// ```
+#[derive(Debug)]
+enum ReadUsernameError {
+    IoError(io::Error),
+    EmptyUsername(String),
+}
+
+impl Error for ReadUsernameError {}
+
+impl Display for ReadUsernameError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::IoError(e) => write!(f, "IO 错误: {e}"),
+            Self::EmptyUsername(filename) => write!(f, "没有找到这个名字 {filename}"),
+        }
+    }
+}
+
+impl From<io::Error> for ReadUsernameError {
+    fn from(err: io::Error) -> ReadUsernameError {
+        ReadUsernameError::IoError(err)
+    }
+}
+
+fn read_username(path: &str) -> Result<String, ReadUsernameError> {
+    let mut username = String::with_capacity(100);
+    File::open(path)?.read_to_string(&mut username)?;
+    if username.is_empty() {
+        return Err(ReadUsernameError::EmptyUsername(String::from(path)));
+    }
+    Ok(username)
+}
+
+fn main() {
+    //fs::write("config.dat", "").unwrap();
+    let username = read_username("config.dat");
+    println!("username or error: {username:?}");
+}

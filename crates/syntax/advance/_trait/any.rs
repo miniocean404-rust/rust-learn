6ddbb0b944f7e2c27d71519dfdb9fa14b72763b6
MiniOcean404 <1037306928@qq.com>
2024-09-05 18:24:33 +0800

    📝 memo: any

---
+++ crates/syntax/advance/_trait/any.rs
@@ -0,0 +1,61 @@
+// 动态 Trait 转为指定类型
+// 动态 Trait 介绍 oo -> dyn_trait
+
+use std::{any::Any, error::Error, sync::Arc};
+
+pub struct DevlopmentMode {
+    name: String,
+}
+
+impl Mode for DevlopmentMode {}
+
+pub struct TestingMode {
+    name: String,
+}
+
+impl Mode for TestingMode {}
+
+// 最好不要实现 Copy 和 Clone，因为它们返回 Self 导致转化不是一个 object-safe trait
+pub trait Mode {}
+
+pub fn use_box_mode<M: Mode + 'static>(mode: M) -> Result<(), Box<dyn Error>> {
+    let mode = Box::new(mode) as Box<dyn Any>;
+
+    if mode.is::<DevlopmentMode>() {
+        let dev = mode.downcast::<DevlopmentMode>().unwrap();
+        println!("Development mode: {}", dev.name);
+    } else {
+        let test = mode.downcast::<TestingMode>().unwrap();
+        println!("Testing mode: {}", test.name);
+    }
+
+    Ok(())
+}
+
+pub fn use_arc_mode<M: Mode + 'static>(mode: M) -> Result<(), Box<dyn Error>> {
+    let mode = Arc::new(mode) as Arc<dyn Any>;
+
+    if mode.is::<DevlopmentMode>() {
+        let dev = mode
+            .downcast_ref::<DevlopmentMode>()
+            .ok_or("Failed to downcast to DevlopmentMode")?;
+
+        println!("Development mode: {}", dev.name);
+    } else {
+        let test = mode
+            .downcast_ref::<TestingMode>()
+            .ok_or("Failed to downcast to TestingMode")?;
+
+        println!("Testing mode: {}", test.name);
+    }
+
+    Ok(())
+}
+
+pub fn print_if_string(value: &dyn Any) {
+    if let Some(s) = value.downcast_ref::<String>() {
+        println!("String ({}): {}", s.len(), s);
+    } else {
+        println!("Not a string");
+    }
+}
--- crates/syntax/advance/_trait/mod.rs
+++ crates/syntax/advance/_trait/mod.rs
@@ -1,3 +1,4 @@
+pub mod any;
 pub mod display;
 pub mod extend_trait;
 pub mod new_type;
